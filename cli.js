e94c44c4d7a11f74ee1cff3656cb6db400c51e25
llambda <xxgsoftware@gmail.com>
2015-02-28 18:35:48 -0600

    Refactor into lib and cli.

---
+++ cli.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+var packagejson = require('./package.json');
+var path = require('path');
+
+program
+.version(packagejson.version)
+.option('-l, --listen [port]', 'TCP port to listen on for HTTP', parseInt)
+.option('-s, --smtp [port]', 'TCP port to listen on for SMTP', parseInt)
+.option('-f, --file [file]', 'File to save SQLite database in')
+
+var RestSmtpSink = require('./index');
+
+var server = new RestSmtpSink(program);
+server.start();
--- index.js
+++ index.js
@@ -1,69 +1,101 @@
+'use strict';
+
 var simplesmtp = require('simplesmtp');
 var fs = require('fs');
-
-var smtpport = process.env.SMTPPORT || 2525;
-var httpport = process.env.HTTPPORT || 2526;
 var compress = require('compression');
+var MailParser = require('mailparser').MailParser;
+var EventEmitter = require('events').EventEmitter;
 
-var MailParser = require("mailparser").MailParser;
+module.exports = RestSmtpSink;
 
-var db = require('knex')({
-	client: 'sqlite3',
-	connection: {
-		filename: './rest-smtp-sink.sqlite'
-	}
-});
+function RestSmtpSink(options) {
+	this.ee = new EventEmitter();
+	this.smtpport = options.smtp || 2525;
+	this.httpport = options.listen || 2526;
+	var filename = options.file || 'rest-smtp-sink.sqlite';
 
-db.schema.createTable('emails', function (table) {
-	table.increments();
-	table.timestamps();
-	table.json('html');
-	table.json('text');
-	table.json('headers');
-	table.json('subject');
-	table.json('messageId');
-	table.json('priority');
-	table.json('from');
-	table.json('to');
-}).then(function (resp) {
-	console.log(resp);
-})
-.catch(function (err) {
-	console.error(err);
-})
+	this.db = require('knex')({
+		client: 'sqlite3',
+		connection: { filename: filename }
+	});
+}
 
-var smtp = simplesmtp.createServer({
-	enableAuthentication: true,
-	requireAuthentication: false,
-	SMTPBanner: 'rest-smtp-sink',
-	disableDNSValidation: true
-});
-smtp.listen(smtpport);
-console.log('SMTP server listening on port ' + smtpport);
+RestSmtpSink.prototype.start = function() {
+	var self = this;
+	return self.createSchema()
+	.then(function () {
+		self.createSmtpSever();
+		self.smtp.listen(self.smtpport);
+		console.log('SMTP server listening on port ' + self.smtpport);
 
-smtp.on("startData", function(connection){
-	connection.mailparser = new MailParser();
-	connection.mailparser.on("end", function(mail_object){
-		db('emails')
-		.insert({
-			"created_at": new Date() ,
-			"updated_at": new Date() ,
-			'html': JSON.stringify(mail_object.html) ,
-			'text': JSON.stringify(mail_object.text) ,
-			'headers': JSON.stringify(mail_object.headers) ,
-			'subject': JSON.stringify(mail_object.subject) ,
-			'messageId': JSON.stringify(mail_object.messageId) ,
-			'priority': JSON.stringify(mail_object.priority) ,
-			'from': JSON.stringify(mail_object.from) ,
-			'to': JSON.stringify(mail_object.to)
-		})
-		.then(function (res) {
-			connection.donecallback(null, res); // ABC1 is the queue id to be advertised to the client
+		self.server = self.createWebServer().listen(self.httpport, function() {
+			console.log('HTTP server listening on port %d', self.httpport);
+		});
+	})	
+}
+
+RestSmtpSink.prototype.createSchema = function () {
+	var self = this;
+	return this.db.schema.createTable('emails', function (table) {
+		table.increments();
+		table.timestamps();
+		table.json('html');
+		table.json('text');
+		table.json('headers');
+		table.json('subject');
+		table.json('messageId');
+		table.json('priority');
+		table.json('from');
+		table.json('to');
+	})
+	.catch(function (err) {
+		console.error(err);
+		console.log(self)
+		self.ee.emit('poop', err);
+	})
+}
+
+RestSmtpSink.prototype.createSmtpSever = function() {
+	this.smtp = simplesmtp.createServer({
+		enableAuthentication: true,
+		requireAuthentication: false,
+		SMTPBanner: 'rest-smtp-sink',
+		disableDNSValidation: true
+	});
+
+	this.smtp.on("startData", function(connection){
+		connection.mailparser = new MailParser();
+		connection.mailparser.on("end", function(mail_object){
+			this.db('emails')
+			.insert({
+				"created_at": new Date() ,
+				"updated_at": new Date() ,
+				'html': JSON.stringify(mail_object.html) ,
+				'text': JSON.stringify(mail_object.text) ,
+				'headers': JSON.stringify(mail_object.headers) ,
+				'subject': JSON.stringify(mail_object.subject) ,
+				'messageId': JSON.stringify(mail_object.messageId) ,
+				'priority': JSON.stringify(mail_object.priority) ,
+				'from': JSON.stringify(mail_object.from) ,
+				'to': JSON.stringify(mail_object.to)
+			})
+			.then(function (res) {
+				connection.donecallback(null, res);
+			});
 		});
 	});
-});
 
-function deserialize(o) {
+	this.smtp.on("data", function(connection, chunk){
+		connection.mailparser.write(chunk);
+	});
+
+	this.smtp.on("dataReady", function(connection, callback){
+		connection.donecallback = callback;
+		connection.mailparser.end();
+	});
+}
+
+RestSmtpSink.prototype.deserialize = function (o) {
 	o.html = JSON.parse(o.html)
 	o.text = JSON.parse(o.text)
 	o.headers = JSON.parse(o.headers)
@@ -76,70 +108,60 @@ function deserialize(o) {
 	return o;
 }
 
+RestSmtpSink.prototype.createWebServer = function () {
+	var self = this;
+	var express = require('express');
+	var app = express();
 
-smtp.on("data", function(connection, chunk){
-	connection.mailparser.write(chunk);
-});
+	app.use(compress());
 
-smtp.on("dataReady", function(connection, callback){
-	connection.donecallback = callback;
-	connection.mailparser.end();
-});
+	app.get('/', function(req, res){
+		res.send('<html><body>HTTP listening on port ' + self.httpport
+			+ '<br>SMTP server listening on port ' + self.smtpport
+			+ '<br><br>API'
+			+ '<br><a href="/api/email">All Emails ( /api/email )</a>'
+			+ '<br><a href="/api/email/latest">Last received Email</a> ( /api/email/latest )'
+			+ '<br><a href="/api/email/1">Email #1</a> ( /api/email/1 )'
+			+ '<br><a href="/api/email/2">Email #2</a> ( /api/email/2 )'
+			+ '</body></html>'
+			)
+	});
 
+	app.get('/api/email', function(req, res, next){
+		self.db.select('*').from('emails')
+		.then(function (resp) {
+			resp.forEach(self.deserialize);
+			res.json(resp);
+		})
+		.catch(next)
+	});
 
-var express = require('express');
-var app = express();
+	app.get('/api/email/latest', function(req, res, next){
+		self.db.select('*').from('emails')
+		.orderBy('id', 'desc')
+		.limit(1)
+		.then(function (resp) {
+			if (resp.length < 1) {
+				res.status(404).send('Not found')
+			} else {
+				res.json(self.deserialize(resp[0]));
+			}
+		})
+		.catch(next)
+	});
 
-app.use(compress());
+	app.get('/api/email/:id', function(req, res, next){
+		self.db.select('*').from('emails')
+		.where('id', '=', req.params.id)
+		.then(function (resp) {
+			if (resp.length < 1) {
+				res.status(404).send('Not found')
+			} else {
+				res.json(self.deserialize(resp[0]));
+			}
+		})
+		.catch(next)
+	});
 
-app.get('/', function(req, res){
-	res.send('<html><body>HTTP listening on port ' + server.address().port
-	+ '<br>SMTP server listening on port ' + smtpport
-	+ '<br><br>API'
-	+ '<br><a href="/api/email">All Emails ( /api/email )</a>'
-	+ '<br><a href="/api/email/latest">Last received Email</a> ( /api/email/latest )'
-	+ '<br><a href="/api/email/1">Email #1</a> ( /api/email/1 )'
-	+ '<br><a href="/api/email/2">Email #2</a> ( /api/email/2 )'
-	+ '</body></html>'
-	)
-});
-
-app.get('/api/email', function(req, res, next){
-	db.select('*').from('emails')
-	.then(function (resp) {
-		resp.forEach(deserialize);
-		res.json(resp);
-	})
-	.catch(next)
-});
-
-app.get('/api/email/latest', function(req, res, next){
-	db.select('*').from('emails')
-	.orderBy('id', 'desc')
-	.limit(1)
-	.then(function (resp) {
-		if (resp.length < 1) {
-			res.status(404).send('Not found')
-		} else {
-			res.json(deserialize(resp[0]));
-		}
-	})
-	.catch(next)
-});
-
-app.get('/api/email/:id', function(req, res, next){
-	db.select('*').from('emails')
-	.where('id', '=', req.params.id)
-	.then(function (resp) {
-		if (resp.length < 1) {
-			res.status(404).send('Not found')
-		} else {
-			res.json(deserialize(resp[0]));
-		}
-	})
-	.catch(next)
-});
-
-var server = app.listen(httpport, function() {
-    console.log('HTTP server listening on port %d', server.address().port);
-});
+	return app;
+}
